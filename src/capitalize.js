151789585ad417faa1a394a1f81931f8ee76b099
Radon4ik <Loboda.Radimir@gmail.com>
2021-12-20 20:54:31 +0300

    edit capitalize.js file

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,4 +1,18 @@
+const string = "test test test";
+
 function capitalize(string) {
+
+    let text = string.split(' ');
+
+
+    const result = text.map(function (item) {
+        return item[0].toUpperCase() + item.slice(1);
+
+    });
+
+    return result.join(' ');
 }
+console.log(capitalize(string));
+
 
 module.exports = capitalize;
